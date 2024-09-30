3c387d1e3c53614fcccb0fed376414578e42494c
Анатолий <reddragon76@yandex.ru>
2024-10-01 00:04:00 +0500

    Исправление ошибок регистрации

--- My_Django/users/models.py
+++ My_Django/users/models.py
@@ -8,19 +8,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     customer_name = models.CharField(max_length=100)  # получатель заказа по умолчанию
-    city = models.ForeignKey(City, on_delete=models.CASCADE)  # город
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)  # город
     address = models.CharField(max_length=100, blank=True) # адрес доставки по умолчанию
     phone = models.CharField(max_length=100, blank=True) # телефон по умолчанию
 
     def __str__(self):
         return f'{self.user.username} Profile'
-
-    def save(self):
-        super().save()
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
